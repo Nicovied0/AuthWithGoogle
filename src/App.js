1dce0464d6c4a3733e1cd7be001a2a5e6fddd8f8
Nicovied0 <briannicolasoviedo@hotmail.com>
2022-12-21 00:03:34 -0300

    add styles and readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { gapi } from "gapi-script";
 
-import './App.css';
-// import GoogleLogin from 'react-google-login';
-
+// let secretoDelCliente = "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 "
 function App() {
+  const [profile, setProfile] = useState();
+
+  const clientId =
+    "76949937496-7dsm07t23ccn8bpba5udrmii6oc8f8mt.apps.googleusercontent.com";
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    };
+    gapi.load("client:auth2", initClient);
+  });
+
+  const onSuccess = (res) => {
+    setProfile(res.profileObj);
+    console.log(res);
+    console.log(res.profileObj);
+  };
+
+  const onFailure = (err) => {
+    console.log("failed", err);
+  };
+  const logOut = () => {
+    setProfile(null);
+  };
+
   return (
     <div className="App">
-      
+      <h2 className="h2">Login With Google & React</h2>
+      {console.log(profile?.name, "soy progile")}
+      {profile && profile !== null && profile !== [] ? (
+        <div>
+          <img src={profile?.imageUrl} alt="user" className="imgLogin" />
+          <div className="div_p">
+            <p>Name: {profile.name}</p>
+            <p>Email Address: {profile.email}</p>
+          </div>
+          <GoogleLogout
+            clientId={clientId}
+            buttonText="Cerrar sesión"
+            onLogoutSuccess={logOut}
+          />
+        </div>
+      ) : (
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Iniciar sesión con Google"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+        />
+      )}
     </div>
   );
 }
